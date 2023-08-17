89eebde6566eead3316652a41f22034c3e115f2e
Oshurkov <oshurkov.oc@gmail.com>
2023-08-17 14:11:38 +0500

    start project

---
+++ src/main/java/ru/skyeng/oshurkov/PostOffice/PostOfficeApplication.java
@@ -0,0 +1,13 @@
+package ru.skyeng.oshurkov.PostOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skyeng/oshurkov/PostOffice/PostOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skyeng.oshurkov.PostOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
